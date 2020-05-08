5619b8594dc1618f5360477c7a8534c8662aea7d
Maryem Souayah <maryemsouayah3@gmail.com>
2020-05-08 02:09:44 +0100

    first commit

---
+++ src/services/adherents.service.js
@@ -0,0 +1,24 @@
+const adherents = [
+    {id :"1",
+    cin:"07480781",
+    nom:"Maryem",
+    prenom:"Souayah",
+    dateNaissance:"1996-12-12", 
+    //etat du compte adherent active ou banni
+    etat :"active"
+    },
+    {id :"2",
+    cin:"07480700",
+    nom:"Intissar",
+    prenom:"Chrigui",
+    dateNaissance:"1995-02-08", 
+    etat :"active"
+    },
+    {id :"3",
+    cin:"07480701",
+    nom:"Eslem",
+    prenom:"khemiri",
+    dateNaissance:"1996-12-23", 
+    etat :"banni"
+    }
+  ]
---
+++ src/services/livres.service.js
@@ -0,0 +1,77 @@
+const books = [
+    {id :"1",
+    //Code-barres EAN
+    EAN:9782714411501,
+    libelle:"Les oiseaux se cachent pour mourir", 
+    auteur:"Colleen McCullough",
+    édition:2010,
+    nbExemplaires:21
+    },
+    {id :"2",
+    EAN:9782266308472,
+    libelle:"Jamais sans ma fille", 
+    auteur:"Betty Mahmoody",
+    édition:2003,
+    nbExemplaires:15
+    },
+    {id :"3",
+    EAN:9782352876113,
+    libelle:"Jane Eyre", 
+    auteur:"Charlotte Brontë",
+    édition:2015,
+    nbExemplaires:10
+    }
+    ,
+    {id :"4",
+    EAN:9782266107532,
+    libelle:"La cité de la joie", 
+    auteur:"Dominique Lapierre",
+    édition:2015,
+    nbExemplaires:10
+    },
+    {id :"5",
+    EAN:9782253005018,
+    libelle:"Le meilleur des Mondes", 
+    auteur:"Aldous Huxley",
+    édition:2017,
+    nbExemplaires:7
+    },
+    {id :"6",
+    EAN:9782070360024,
+    libelle:"L'étranger", 
+    auteur:"Albert Camus",
+    édition:2001,
+    nbExemplaires:11
+    },
+    {id :"7",
+    EAN:9782203135215,
+    libelle:"Les malheurs de Sophie", 
+    auteur:"Comtesse de Ségur",
+    édition:2004,
+    nbExemplaires:18
+    },
+    {id :"8",
+    EAN:9782070413119,
+    libelle:"Madame Bovary", 
+    auteur:"Gustave Flaubert",
+    édition:2006,
+    nbExemplaires:25
+    },
+    {id :"9",
+    EAN:9782253006220,
+    libelle:"La Mère", 
+    auteur:"Pearl Buck",
+    édition:2007,
+    nbExemplaires:9
+    }
+    ,
+    {id :"10",
+    EAN:9782290231241,
+    libelle:"Racines", 
+    auteur:"Alex Haley",
+    édition:2011,
+    nbExemplaires:17
+    }
+  ]
+
+ 
